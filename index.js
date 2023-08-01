284d85176983c40e33005e7ba42dde412cc9b0d8
Jakub Adamski <jakub.adamski12@gmail.com>
2023-08-01 12:17:54 +0100

    Edit index.js

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-console.log("hello.world");
+var choices = ["ROCK", "PAPER", "SCISSORS"];
+
+
+
+function getComputerChoice(){
+    var randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
+}
+
+function checkWinner(playerChoice, computerChoice){
+    if(playerChoice == computerChoice){
+    return ("It is tie");
+        
+    }
+    else if(playerChoice == "ROCK" && computerChoice == "PAPER" ||
+            playerChoice == "PAPER" && computerChoice == "ROCK" ||
+            playerChoice == "SCISSORS" && computerChoice == "PAPER"){
+            return ("You won");
+            }
+
+    else{
+        return ("You lost");
+    }
+    
+}
+
+function game(playerChoice, computerChoice){
+    var result = checkWinner(playerChoice, computerChoice);
+    if (result == "It is tie"){
+        return("TIE");
+    }
+    else if(result == "You won"){
+        return ("WIN");
+    }
+    else{
+        return("LOST");
+    }
+
+
+}
+var playerChoice = "ROCK";
+var computerChoice = getComputerChoice();
+console.log(game(playerChoice, computerChoice));
+
+
+
+
+
+
