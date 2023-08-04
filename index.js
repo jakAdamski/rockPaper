3a349bd55a930d33aeb2645d4cb6de1627884540
Jakub Adamski <jakub.adamski12@gmail.com>
2023-08-04 17:32:56 +0100

    edit

--- index.js
+++ index.js
@@ -1,61 +1,71 @@
-var choices = document.querySelectorAll(".choice").addEventListener("click", () {
-    if(innerHTML=="ROCK")
-    {
-    return playerChoice == "ROCK";
-    }
-    else if(innerHTML=="PAPER"){
-        return playerChoice =="PAPER";
-    }
-    else{
-        return playerChoice =="SCISSORS";
-    }
-
-});
+var userScore = 0;
+var computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
 
-
+function main(){
 function getComputerChoice(){
-    var randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    return randomChoice;
+    const choices = ["r","p","s"];
+    const randomNum= Math.floor(Math.random() * choices.length)
+    return choices [randomNum];
+}
+function win(userChoice,computerChoice){
+    userScore++
+    userScore_span.innerHTML = userScore;
+    result_div.innerHTML = userChoice + "beats " + computerChoice + ". You win!";
+}
+function lose(){
+
+    computerScore ++;
+    computerScore_span.innerHTML = computerScore;
+}
+function draw(){
+ document.result.innerHTML="DRAW";
 }
 
-function checkWinner(playerChoice, computerChoice){
-    if(playerChoice == computerChoice){
-    return ("It is tie");
-        
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win();
+            break;
+        case "pr":
+        case "rp":
+        case "ps":
+            lose();
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw();
+            break;    
     }
-    else if(playerChoice == "ROCK" && computerChoice == "PAPER" ||
-            playerChoice == "PAPER" && computerChoice == "ROCK" ||
-            playerChoice == "SCISSORS" && computerChoice == "PAPER"){
-            return ("You won");
-            }
+}
 
-    else{
-        return ("You lost");
-    }
+
+
+    rock_div.addEventListener('click', function(){
+        game("r");
+
+    })
+
+    paper_div.addEventListener('click', function(){
+        game("p");
     
-}
-
-function game(playerChoice, computerChoice){
-    var result = checkWinner(playerChoice, computerChoice);
-    if (result == "It is tie"){
-        return("TIE");
-    }
-    else if(result == "You won"){
-        return ("WIN");
-    }
-    else{
-        return("LOST");
-    }
-
-
-}
-var playerChoice = playerChoice;
-var computerChoice = getComputerChoice();
-console.log(game(playerChoice, computerChoice));
-
-
-
-
-
-
+    })
+    
+    scissors_div.addEventListener('click', function(){
+        game("s");
+        
+        })
+        
+}    
+main();
